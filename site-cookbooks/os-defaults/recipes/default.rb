0cd37957e883ef7a2473423252e4724fa29463d6
itani-hiroki <itanihiroki@gmail.com>
2013-07-16 11:34:18 +0900

    不要なサービス停止

---
+++ site-cookbooks/os-defaults/recipes/default.rb
@@ -0,0 +1,15 @@
+#
+# Cookbook Name:: os-defaults
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+## 不要な各種サービスを停止する
+%W(acpid apmd autofs avahi-daemon avahi-dnsconfd bluetooth cups gpm haldaemon hidd ip6tables kudzu mcstrans mdmonitor microcode_ctl netfs nfslock pcscd portmap readahead_early restorecond rpcgssd rpcidmapd smartd xfs).each do |name|
+  service name do
+    action [ :disable, :stop ]
+  end
+end
