7abe045c365da0823bdd1dd16b76687766282c07
itani-hiroki <itanihiroki@gmail.com>
2013-07-18 10:48:12 +0900

    create subversion cookbook

---
+++ site-cookbooks/subversion/recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: subversion
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package 'subversion' do
+  action :install
+end
