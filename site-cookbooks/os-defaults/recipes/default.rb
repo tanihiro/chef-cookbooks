632d65b0775872f22c2e3c558a2e523f6aea82d3
itani-hiroki <itanihiroki@gmail.com>
2013-07-19 16:16:50 +0900

    os-defaults 修正

--- site-cookbooks/os-defaults/recipes/default.rb
+++ site-cookbooks/os-defaults/recipes/default.rb
@@ -7,6 +7,13 @@
 # All rights reserved - Do Not Redistribute
 #
 
+# 必要なコンパイラなどをインストール   
+%w{gcc gcc-c++ make libxml2 libxml2-devel libxslt libxslt-devel openssl-devel zlib-devel}.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
 ## 不要な各種サービスを停止する
 %W(acpid apmd autofs avahi-daemon avahi-dnsconfd bluetooth cups gpm haldaemon hidd ip6tables kudzu mcstrans mdmonitor microcode_ctl netfs nfslock pcscd portmap readahead_early restorecond rpcgssd rpcidmapd smartd xfs).each do |name|
   service name do
